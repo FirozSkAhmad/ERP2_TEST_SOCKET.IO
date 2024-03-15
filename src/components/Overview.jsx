9037a5510916f52f15af1bae38d255c5dce26c8d
SarikAnwar01 <sarikanwar007@gmail.com>
2024-03-16 00:36:02 +0545

    OverviewApi Integrated

--- src/components/Overview.jsx
+++ src/components/Overview.jsx
@@ -1,43 +1,80 @@
-import React, { useEffect, useState } from 'react'
-import './overview.css'
-import income from '../assets/income.svg';
-import balanceImage from '../assets/balance.svg';
-import expenses from '../assets/expenses.svg';
-import overViewData from '../data/overviewData';
+import React, { useEffect, useState } from "react";
+import "./overview.css";
+import income from "../assets/income.svg";
+import balanceImage from "../assets/balance.svg";
+import expenses from "../assets/expenses.svg";
 
 const Overview = () => {
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  const BaseURL = "https://erp-phase2-bck.onrender.com";
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const accessToken = localStorage.getItem("token");
+        const response = await fetch(`${BaseURL}/overview/getOverview`, {
+          headers: {
+            Authorization: `Bearer ${accessToken}`,
+          },
+        });
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        const responseData = await response.json();
+        setData(responseData);
+
+        setLoading(false);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, []);
+
   return (
-      <div className='over-det'>
-        <div className='over-sec'>
-          <div className='over_Head'>
-              <h3>Overview</h3>
-          </div>
-          <div className='over_Data-fld'>
-            <div className='over_Data'>
-              <img src={income} alt="Income" />
-              <div className='over_Data_amt'>
-                <p className='amt-type'>Income</p>
-                <p className='amt'>$999</p>
+    <div className="over-det">
+      <div className="over-sec">
+        <div className="over_Head">
+          <h3>Overview</h3>
+        </div>
+        <div className="over_Data-fld">
+          {loading ? (
+            <p>Loading...</p>
+          ) : data ? (
+            <>
+              <div className="over_Data">
+                <img src={income} alt="Income" />
+                <div className="over_Data_amt">
+                  <p className="amt-type">Income</p>
+                  <p className="amt">${data?.leads.income}</p>
+                </div>
               </div>
-            </div>
-            <div className='over_Data'>
-              <img src={expenses} alt="" />
-              <div className='over_Data_amt'>
-                <p className='amt-type'>Expenses</p>
-                <p className='amt'>$999</p>
+              <div className="over_Data">
+                <img src={expenses} alt="Expenses" />
+                <div className="over_Data_amt">
+                  <p className="amt-type">Expenses</p>
+                  <p className="amt">${data?.leads.expenses}</p>
+                </div>
               </div>
-            </div>
-            <div className='over_Data'>
-              <img src={balanceImage} alt="" />
-              <div className='over_Data_amt'>
-                <p className='amt-type'>Balance</p>
-                <p className='amt'>$999</p>
+              <div className="over_Data">
+                <img src={balanceImage} alt="Balance" />
+                <div className="over_Data_amt">
+                  <p className="amt-type">Balance</p>
+                  <p className="amt">${data?.leads.balance}</p>
+                </div>
               </div>
-            </div>
-          </div>
+            </>
+          ) : (
+            <p>Error fetching data</p>
+          )}
         </div>
       </div>
-  )
-}
+    </div>
+  );
+};
 
-export default Overview
+export default Overview;
