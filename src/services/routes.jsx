c29c7ba82a42ca82b2282488b5c5739d42773997
SuraZz007 <surajdev9886@gmail.com>
2024-03-15 14:29:06 +0530

    add some extra components

--- src/services/routes.jsx
+++ src/services/routes.jsx
@@ -8,17 +8,18 @@ import OnBoarding from "../components/sales-channel/OnBoarding";
 import History from "../components/sales-channel/History";
 import ChannelDash from "../pages/ChannelDash";
 import Pending from "../components/admin/approval/Pending";
-import PendingReceipts from "../components/admin/Receipts/PendingReceipts";
+import PendingReceipts from "../components/Receipts/PendingReceipts";
 import PayRollCard from "../components/admin/payroll/PayRollCard";
 import Customer from "../components/admin/customer/Customer";
-import MiscellaneousCard from '../components/admin/miscellaneous/MiscellaneousCard'
+import MiscellaneousCard from "../components/admin/miscellaneous/MiscellaneousCard";
 import Discount from "../components/admin/discount/Discount";
-import SpHistory from "../components/admin/sp-history/SpHistory";
-import CpHistory from "../components/admin/cp-history/CpHistory";
+import SpHistory from "../components/sp-history/SpHistory";
+import CpHistory from "../components/cp-history/CpHistory";
 import LeadGeneration from "../components/admin/lead-generation/LeadGeneration";
-import Expenses from "../components/admin/expenses/Expenses";
+import Expenses from "../components/expenses/Expenses";
 import Commissions from "../components/admin/commissions/Commissions";
 import Payments from "../components/admin/payments/Payments";
+import ManagerDash from "../pages/ManagerDash";
 
 const router = createBrowserRouter([
   {
@@ -27,25 +28,30 @@ const router = createBrowserRouter([
     children: [
       { index: true, element: <HomeLogin /> },
       { path: "signup", element: <SignUp /> },
-      { path: "/admin/dashboard", element:  <AdminDash />},
-      { path: "/admin/approvals", element:  <Pending />},
-      { path: "/admin/receipts", element:  <PendingReceipts />},
-      { path: "/admin/payments", element:  <Payments />},
-      { path: "/admin/payroll", element:  <PayRollCard />},
-      { path: "/admin/expenses", element:  <Expenses />},
-      { path: "/admin/commissions", element:  <Commissions />},
-      { path: "/admin/customer", element:  <Customer />},
-      { path: "/admin/discount", element:  <Discount />},
-      { path: "/admin/miscellaneous", element:  <MiscellaneousCard />},
-      { path: "/admin/sp-history", element:  <SpHistory />},
-      { path: "/admin/cp-history", element:  <CpHistory />},
-      { path: "/admin/lead-generation", element:  <LeadGeneration />},
-      { path: "/sales/dashboard", element:  <SalesDash />},
-      { path: "/sales/onBoard", element:  <OnBoarding />},
-      { path: "/sales/history", element:  <History />},
-      { path: "/channel/dashboard", element:  <ChannelDash />},
-      { path: "/channel/onBoard", element:  <OnBoarding />},
-      { path: "/channel/history", element:  <History />},
+      { path: "/admin/dashboard", element: <AdminDash /> },
+      { path: "/admin/approvals", element: <Pending /> },
+      { path: "/admin/receipts", element: <PendingReceipts /> },
+      { path: "/admin/payments", element: <Payments /> },
+      { path: "/admin/payroll", element: <PayRollCard /> },
+      { path: "/admin/expenses", element: <Expenses /> },
+      { path: "/admin/commissions", element: <Commissions /> },
+      { path: "/admin/customer", element: <Customer /> },
+      { path: "/admin/discount", element: <Discount /> },
+      { path: "/admin/miscellaneous", element: <MiscellaneousCard /> },
+      { path: "/admin/sp-history", element: <SpHistory /> },
+      { path: "/admin/cp-history", element: <CpHistory /> },
+      { path: "/admin/lead-generation", element: <LeadGeneration /> },
+      { path: "/manager/dashboard", element: <ManagerDash /> },
+      { path: "/manager/receipts", element: <PendingReceipts /> },
+      { path: "/manager/expenses", element: <Expenses /> },
+      { path: "/manager/sp-history", element: <SpHistory /> },
+      { path: "/manager/cp-history", element: <CpHistory /> },
+      { path: "/sales/dashboard", element: <SalesDash /> },
+      { path: "/sales/onBoard", element: <OnBoarding /> },
+      { path: "/sales/history", element: <History /> },
+      { path: "/channel/dashboard", element: <ChannelDash /> },
+      { path: "/channel/onBoard", element: <OnBoarding /> },
+      { path: "/channel/history", element: <History /> },
     ],
   },
 ]);
