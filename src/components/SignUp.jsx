7159d5bd2213afdea8fa79e3d0b255b6fdb5bf09
SarikAnwar01 <sarikanwar007@gmail.com>
2024-03-15 23:54:03 +0545

    update(signup)

--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -1,131 +1,239 @@
 import { useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import "../index.css";
 
 const SignUp = () => {
-  const [formData, setFormData] = useState({ role_type: "" });
+  const navigate = useNavigate();
+  const [formData, setFormData] = useState({
+    user_name: "",
+    email_id: "",
+    password: "",
+    confirmpassword: "",
+    role_type: "",
+    address: "",
+    contact_no: "",
+    pancard_no: "",
+    bank_ac_no: "",
+    bussiness_experience: "",
+  });
+  const BaseURL = " https://erp-phase2-bck.onrender.com";
 
-  const handleRoleChange = (e) => {
-    setFormData({ ...formData, role_type: e.target.value });
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
   };
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    console.log(formData);
+    try {
+      const response = await fetch(
+        `https://erp-phase2-bck.onrender.com/auth/register`,
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(formData),
+        }
+      );
 
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+
+      // Handle success response here
+      console.log("Registration successful");
+      // Redirect user based on their role
+      switch (formData.role_type) {
+        case "SUPER ADMIN":
+          navigate("/admin/dashboard");
+          break;
+        case "MANAGER":
+          navigate("/manager/dashboard");
+          break;
+        case "CHANNEL PARTNER":
+          navigate("/channel/dashboard");
+          break;
+        case "SALES PERSON":
+          navigate("/sales/dashboard");
+          break;
+        default:
+          // Redirect to a default page if role is not recognized
+          navigate("/");
+      }
+    } catch (error) {
+      // Handle error here
+      console.error("Error occurred during registration:", error.message);
+      // You might show an error message to the user
+    }
+  };
   return (
-        <div className="signUp">
-          <div className="log__Title">
-            <h3>Create an account</h3>
+    <div className="signUp">
+      <div className="log__Title">
+        <h3>Create an account</h3>
+      </div>
+      <div className="log__Form">
+        <form onSubmit={handleSubmit}>
+          <div className="form_input-field">
+            <label htmlFor="name" className="form-label">
+              Name*
+            </label>
+            <input
+              type="text"
+              id="name"
+              placeholder="Enter your name"
+              name="user_name"
+              value={formData.user_name}
+              onChange={handleInputChange}
+            />
           </div>
-          <div className="log__Form">
-            <form>
+          <div className="form_input-field">
+            <label htmlFor="email" className="form-label">
+              Email*
+            </label>
+            <input
+              type="email"
+              id="email"
+              placeholder="Enter your email"
+              name="email_id"
+              value={formData.email_id}
+              onChange={handleInputChange}
+            />
+          </div>
+          <div className="form_input-field">
+            <label htmlFor="password" className="form-label">
+              Password*
+            </label>
+            <input
+              type="password"
+              id="password"
+              placeholder="Create a password"
+              name="password"
+              value={formData.password}
+              onChange={handleInputChange}
+            />
+          </div>
+          <div className="form_input-field">
+            <label htmlFor="password" className="form-label">
+              Confirm Password*
+            </label>
+            <input
+              type="password"
+              id="password"
+              placeholder="Create a password"
+              name="confirmpassword"
+              value={formData.confirmpassword}
+              onChange={handleInputChange}
+            />
+          </div>
+          <div className="form_input-field">
+            <label htmlFor="role" className="form-label">
+              Role*
+            </label>
+            <select
+              id="role"
+              className="form-label"
+              name="role_type"
+              value={formData.role_type}
+              onChange={handleInputChange}
+            >
+              <option id="" value="" style={{ fontSize: "14px" }}>
+                Select your role
+              </option>
+              <option value="SUPER ADMIN" style={{ fontSize: "14px" }}>
+                Super Admin
+              </option>
+              <option value="MANAGER" style={{ fontSize: "14px" }}>
+                Manager
+              </option>
+              <option value="CHANNEL PARTNER" style={{ fontSize: "14px" }}>
+                Channel Partner
+              </option>
+              <option value="SALES PERSON" style={{ fontSize: "14px" }}>
+                Sales Person
+              </option>
+            </select>
+          </div>
+          {formData.role_type === "channel-partner" && (
+            <>
               <div className="form_input-field">
-                <label htmlFor="name" className="form-label">
-                  Name*
-                </label>
-                <input type="text" id="name" placeholder="Enter your name" />
-              </div>
-              <div className="form_input-field">
-                <label htmlFor="email" className="form-label">
-                  Email*
-                </label>
-                <input type="email" id="email" placeholder="Enter your email" />
-              </div>
-              <div className="form_input-field">
-                <label htmlFor="password" className="form-label">
-                  Password*
+                <label htmlFor="address" className="form-label">
+                  Address
                 </label>
                 <input
-                  type="password"
-                  id="password"
-                  placeholder="Create a password"
+                  type="text"
+                  id="address"
+                  placeholder="Address"
+                  name="address"
+                  value={formData.address}
+                  onChange={handleInputChange}
                 />
               </div>
               <div className="form_input-field">
-                <label htmlFor="role" className="form-label">
-                  Role*
+                <label htmlFor="contact" className="form-label">
+                  Contact Number
                 </label>
-                <select
-                  id="role"
-                  className="form-label"
-                  value={formData.role_type}
-                  onChange={handleRoleChange}
-                >
-                  <option id="" value="" style={{ fontSize: "14px" }}>
-                    Select your role
-                  </option>
-                  <option value="super-admin" style={{ fontSize: "14px" }}>
-                    Super Admin
-                  </option>
-                  <option value="manager" style={{ fontSize: "14px" }}>
-                    Manager
-                  </option>
-                  <option value="channel-partner" style={{ fontSize: "14px" }}>
-                    Channel Partner
-                  </option>
-                  <option value="sales-person" style={{ fontSize: "14px" }}>
-                    Sales Person
-                  </option>
-                </select>
+                <input
+                  type="tel"
+                  id="contact"
+                  placeholder="Enter Contact Number"
+                  name="contact_no"
+                  value={formData.contact_no}
+                  onChange={handleInputChange}
+                />
               </div>
-              {formData.role_type === "channel-partner" && (
-                <>
-                  <div className="form_input-field">
-                    <label htmlFor="address" className="form-label">
-                      Address
-                    </label>
-                    <input type="text" id="address" placeholder="Address" />
-                  </div>
-                  <div className="form_input-field">
-                    <label htmlFor="contact" className="form-label">
-                      Contact Number
-                    </label>
-                    <input
-                      type="tel"
-                      id="contact"
-                      placeholder="Enter Contact Number"
-                    />
-                  </div>
-                  <div className="form_input-field">
-                    <label htmlFor="pancard" className="form-label">
-                      Pancard Number
-                    </label>
-                    <input
-                      type="text"
-                      id="pancard"
-                      placeholder="Enter Pancard Number"
-                    />
-                  </div>
-                  <div className="form_input-field">
-                    <label htmlFor="account" className="form-label">
-                      Bank A/C Number
-                    </label>
-                    <input
-                      type="number"
-                      id="account"
-                      placeholder="Enter Bank A/C Number"
-                    />
-                  </div>
-                  <div className="form_input-field">
-                    <label htmlFor="experience" className="form-label">
-                      Business Experience
-                    </label>
-                    <input
-                      type="text"
-                      id="experience"
-                      placeholder="Enter Business Experience"
-                    />
-                  </div>
-                </>
-              )}
-              <div className="sbt_btn">
-                <button type="submit">Get Started</button>
+              <div className="form_input-field">
+                <label htmlFor="pancard" className="form-label">
+                  Pancard Number
+                </label>
+                <input
+                  type="text"
+                  id="pancard"
+                  placeholder="Enter Pancard Number"
+                  name="pancard_no"
+                  value={formData.pancard_no}
+                  onChange={handleInputChange}
+                />
               </div>
-              <div className="form_sign-up">
-                <span>
-                  Already have an account? <Link to="/">Sign In</Link>
-                </span>
+              <div className="form_input-field">
+                <label htmlFor="account" className="form-label">
+                  Bank A/C Number
+                </label>
+                <input
+                  type="text"
+                  id="account"
+                  placeholder="Enter Bank A/C Number"
+                  name="bank_ac_no"
+                  value={formData.bank_ac_no}
+                  onChange={handleInputChange}
+                />
               </div>
-            </form>
+              <div className="form_input-field">
+                <label htmlFor="experience" className="form-label">
+                  Business Experience
+                </label>
+                <input
+                  type="text"
+                  id="experience"
+                  placeholder="Enter Business Experience"
+                  name="bussiness_experience"
+                  value={formData.bussiness_experience}
+                  onChange={handleInputChange}
+                />
+              </div>
+            </>
+          )}
+          <div className="sbt_btn">
+            <button type="submit">Get Started</button>
           </div>
-        </div>
+          <div className="form_sign-up">
+            <span>
+              Already have an account? <Link to="/">Sign In</Link>
+            </span>
+          </div>
+        </form>
+      </div>
+    </div>
   );
 };
 
