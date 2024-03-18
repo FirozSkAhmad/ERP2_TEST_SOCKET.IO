e17f8fe470e4b54c2c411f07de9b6b07abedf754
SuraZz007 <surajdev9886@gmail.com>
2024-03-18 17:32:39 +0530

    add dependency causing bug

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -54,7 +54,7 @@ const Table = ({ selectedButton }) => {
         (project) => project.project_type === selectedButton.toUpperCase()
       )
     );
-  }, [selectedButton]);
+  }, [selectedButton, scaleData]);
 
   useEffect(() => {
     const handleResize = () => {
