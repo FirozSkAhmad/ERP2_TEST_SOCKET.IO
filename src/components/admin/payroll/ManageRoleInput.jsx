7d79be61ff427d4b20a9b7d21960ff8c1314c653
Bimal Saggu <bimalsaggu28@gmail..com>
2024-03-20 18:13:37 +0530

    add Payroll API

--- src/components/admin/payroll/ManageRoleInput.jsx
+++ src/components/admin/payroll/ManageRoleInput.jsx
@@ -1,14 +1,46 @@
 import React from "react";
-import './payrollCard.css'
+import './payrollCard.css';
+import { useNavigate } from "react-router-dom";
 
 const ManageRoleInput = () => {
+    const navigate = useNavigate();
+
+    const BaseURL = "https://erp-phase2-bck.onrender.com";
+
+    // Add New Role Type
+
+    const handleRoleSubmit = async (e) => {
+        e.preventDefault();
+
+        try {
+          const accessToken = localStorage.getItem("token");
+          const roleType = e.target.role.value;
+          const response = await fetch(`${BaseURL}/payroll/addRoleType`, {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+              Authorization: `Bearer ${accessToken}`,
+            },
+            body: JSON.stringify({ role_type: roleType }),
+          });
+          if (!response.ok) {
+            throw new Error("Network error. Network response was not ok");
+        }
+        console.log("Successfully added new role",  roleType);
+        } catch (error) {
+          console.error("Error submitting new role type:", error);
+        }
+
+        navigate("/admin/payroll");
+    };
+
   return (
     <div className="payroll">
             <div className="payroll-sec">
                 <div className="payroll-form">
-                    <form action="">
+                    <form onSubmit={handleRoleSubmit}>
                         <div className="manage-input">
-                            <input type="text" placeholder="Add Role" />
+                            <input type="text" name="role" placeholder="Add Role" />
                             <div className="manage-sbt">
                                 <button>Submit</button>
                             </div>
